41ed4b21c325b2d2bccac8478d8bf0814146cf47
Javidan <iamthejacks0l0@gmail.com>
2025-02-17 19:34:24 +0400

    modified:   app/__pycache__/settings.cpython-313.pyc 	modified:   app/settings.py 	modified:   contents/__pycache__/admin.cpython-313.pyc 	modified:   contents/__pycache__/models.cpython-313.pyc 	modified:   db.sqlite3 	modified:   static/admin/js/cancel.js 	modified:   static/admin/js/popup_response.js 	modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	new file:   static/jazzmin/css/main.css 	new file:   static/jazzmin/img/calendar-icons.svg 	new file:   static/jazzmin/img/default-log.svg 	new file:   static/jazzmin/img/default.jpg 	new file:   static/jazzmin/img/icon-calendar.svg 	new file:   static/jazzmin/img/icon-changelink.svg 	new file:   static/jazzmin/img/selector-icons.svg 	new file:   static/jazzmin/js/change_form.js 	new file:   static/jazzmin/js/change_list.js 	new file:   static/jazzmin/js/main.js 	new file:   static/jazzmin/js/related-modal.js 	new file:   static/jazzmin/js/ui-builder.js 	new file:   static/jazzmin/plugins/bootstrap-show-modal/bootstrap-show-modal.min.js 	new file:   static/media/pen.png 	new file:   static/vendor/adminlte/css/adminlte.min.css 	new file:   static/vendor/adminlte/css/adminlte.min.css.map 	new file:   static/vendor/adminlte/img/AdminLTELogo.png 	new file:   static/vendor/adminlte/img/icons.png 	new file:   static/vendor/adminlte/img/user2-160x160.jpg 	new file:   static/vendor/adminlte/js/adminlte.min.js 	new file:   static/vendor/adminlte/js/adminlte.min.js.map 	new file:   static/vendor/bootstrap/js/bootstrap.min.js 	new file:   static/vendor/bootstrap/js/bootstrap.min.js.map 	new file:   static/vendor/bootswatch/cerulean/bootstrap.min.css 	new file:   static/vendor/bootswatch/cosmo/bootstrap.min.css 	new file:   static/vendor/bootswatch/cyborg/bootstrap.min.css 	new file:   static/vendor/bootswatch/darkly/bootstrap.min.css 	new file:   static/vendor/bootswatch/default/bootstrap.min.css 	new file:   static/vendor/bootswatch/flatly/bootstrap.min.css 	new file:   static/vendor/bootswatch/journal/bootstrap.min.css 	new file:   static/vendor/bootswatch/litera/bootstrap.min.css 	new file:   static/vendor/bootswatch/lumen/bootstrap.min.css 	new file:   static/vendor/bootswatch/lux/bootstrap.min.css 	new file:   static/vendor/bootswatch/materia/bootstrap.min.css 	new file:   static/vendor/bootswatch/minty/bootstrap.min.css 	new file:   static/vendor/bootswatch/pulse/bootstrap.min.css 	new file:   static/vendor/bootswatch/sandstone/bootstrap.min.css 	new file:   static/vendor/bootswatch/simplex/bootstrap.min.css 	new file:   static/vendor/bootswatch/sketchy/bootstrap.min.css 	new file:   static/vendor/bootswatch/slate/bootstrap.min.css 	new file:   static/vendor/bootswatch/solar/bootstrap.min.css 	new file:   static/vendor/bootswatch/spacelab/bootstrap.min.css 	new file:   static/vendor/bootswatch/superhero/bootstrap.min.css 	new file:   static/vendor/bootswatch/united/bootstrap.min.css 	new file:   static/vendor/bootswatch/yeti/bootstrap.min.css 	new file:   static/vendor/fontawesome-free/css/all.min.css 	new file:   static/vendor/fontawesome-free/webfonts/fa-brands-400.ttf 	new file:   static/vendor/fontawesome-free/webfonts/fa-brands-400.woff2 	new file:   static/vendor/fontawesome-free/webfonts/fa-regular-400.ttf 	new file:   static/vendor/fontawesome-free/webfonts/fa-regular-400.woff2 	new file:   static/vendor/fontawesome-free/webfonts/fa-solid-900.ttf 	new file:   static/vendor/fontawesome-free/webfonts/fa-solid-900.woff2 	new file:   static/vendor/fontawesome-free/webfonts/fa-v4compatibility.ttf 	new file:   static/vendor/fontawesome-free/webfonts/fa-v4compatibility.woff2 	new file:   static/vendor/select2/css/select2.min.css 	new file:   static/vendor/select2/js/select2.min.js 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	new file:   staticfiles/media/pen.png

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,7 @@ SECRET_KEY = 'django-insecure-(v=n27d&4ix2)zi0j*85h+h@d_4!+k@(006az8s+ct#398gs&6
 DEBUG = True
 ALLOWED_HOSTS = ["*","vercel.app"]
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,4 +75,59 @@ CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
     },
+}
+JAZZMIN_SETTINGS = {
+    "site_title": "Data Admin",
+    "site_header": "Data",
+    "site_brand": "Data",
+    "site_logo": "media/logo.png",
+    "login_logo": None,
+    "login_logo_dark": None,
+    "site_logo_classes": "img-circle",
+    "site_icon": "media/logo.png",
+    "welcome_sign": "Welcome to the Data's Admin-Panel",
+    "copyright": "Created by Javidan Babayev",
+    "user_avatar": None,
+    "topmenu_links": [
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"app": "books"},
+    ],
+    "usermenu_links": [
+        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"model": "auth.user"}
+    ],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "hide_apps": [],
+    "hide_models": [],
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    "custom_links": {
+        "books": [{
+            "name": "Make Messages", 
+            "url": "make_messages", 
+            "icon": "fas fa-comments",
+            "permissions": ["books.view_book"]
+        }]
+    },
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "contents.Reviews": "fas fa-pencil-alt",
+        "contents.Projects": "fas fa-project-diagram",
+        "contents.Blog": "fas fa-newspaper",
+        "contents.Services": "fas fa-cogs",
+        "main.HeaderWall": "fas fa-images",
+    },
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": False,
+    "custom_css": 'css/style.css',
+    "custom_js": 'js/app.js',
+    "use_google_fonts_cdn": True,
+    "show_ui_builder": False,
+    "changeform_format": "horizontal_tabs",
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    "language_chooser": False,
 }
